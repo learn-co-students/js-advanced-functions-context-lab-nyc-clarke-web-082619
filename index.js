5e353df2292c4787d2b48857df109aa6d7f3d1f3
github username <github email address>
2019-10-16 16:54:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 /* Your Code Here */
+function createEmployeeRecord(recordArray) {
+    return {
+        firstName : recordArray[0],
+        familyName : recordArray[1],
+        title: recordArray[2],
+        payPerHour: recordArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(recordsArray) {
+    return recordsArray.map(record => createEmployeeRecord(record));
+}
+
+function createTimeInEvent (date) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(date.split(" ")[1]),  
+        date: date.split(" ")[0]
+    });
+    return this;    
+}
+
+function createTimeOutEvent(date)  {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(date.split(" ")[1]),  
+        date: date.split(" ")[0]
+    });
+    return this;
+}
+
+
+function hoursWorkedOnDate(date){
+    let checkIn = this.timeInEvents.find(d => d.date === date);
+    let checkOut = this.timeOutEvents.find(d => d.date === date);
+    return (checkOut.hour - checkIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this,date) * this.payPerHour;    
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +62,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(recordsArray){
+    let sum = 0;
+    recordsArray.forEach(record => sum += allWagesFor.call(record))
+    return sum;
+}
+
+function findEmployeeByFirstName (recordsArray, firstName) {
+    return recordsArray.find(record => record.firstName === firstName);
 }
