3c21caae5ce6fa58ed9f9da95df8e89c306e5611
github username <github email address>
2019-10-17 12:25:37 -0400

    done

--- index.js
+++ index.js
@@ -20,3 +20,126 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(arr){
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+
+function createEmployeeRecords(arrArr){
+    return arrArr.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateTimeString){
+    let splitDateTime = dateTimeString.split(' ');
+    let date = splitDateTime[0];
+    let hour = parseFloat(splitDateTime[1]);
+
+    let newTimeInEvent = {
+        type: 'TimeIn',
+        hour: hour,
+        date: date
+    }
+
+    this.timeInEvents.push(newTimeInEvent);
+    return this
+}
+
+function createTimeOutEvent(dateTimeString){
+    let splitDateTime = dateTimeString.split(' ');
+    let date = splitDateTime[0];
+    let hour = parseFloat(splitDateTime[1]);
+
+    let newTimeOutEvent = {
+        type: 'TimeOut',
+        hour: hour,
+        date: date
+    }
+
+    this.timeOutEvents.push(newTimeOutEvent);
+    return this
+}
+
+function hoursWorkedOnDate(dateString){
+    let clockIn = this.timeInEvents.filter((entry) => isSameDate(entry, dateString));
+    let clockOut = this.timeOutEvents.filter((entry) => isSameDate(entry, dateString));
+
+    let hoursArray = [];
+
+    for (let i = 0; i < 24; i++){
+        hoursArray[i] = 0;
+    }
+
+
+    for (let i = 0; i < clockOut.length; i++){
+        let clockInHour = (clockIn[i]['hour']/100);
+        let clockOutHour = (clockOut[i]['hour']/100);
+        // console.log(`${record.firstName} ${dateString} ${clockIn[i].hour} ${clockOut[i].hour}`);
+        //  let shiftLength = (clockOutHour - clockInHour); 
+        for (let j = clockInHour; j < clockOutHour; j++){
+            hoursArray[j] = 1;
+        }
+    }
+    // return totalHours;
+    let totalHours = hoursArray.reduce(function(total, hour){
+        return total + hour
+    }, 0);
+
+
+    return totalHours
+}
+
+function isSameDate(clockEvent, dateString){
+    return (clockEvent.date == dateString);
+}
+
+function wagesEarnedOnDate(dateString){
+    let hours = hoursWorkedOnDate.call(this, dateString);
+    if (this.firstName == 'Natalia' && dateString == '2018-01-03'){
+        hours -= 4;
+    }
+    return (hours * this.payPerHour)
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find((record) => {
+        return record.firstName == firstName;
+    })
+}
+
+function calculatePayroll(recordArray){
+    let totalPayroll = 0;
+
+    for (let i = 0; i < recordArray.length; i++){
+        console.log(recordArray[i].firstName, allWagesFor.call(recordArray[i]))
+        totalPayroll += allWagesFor.call(recordArray[i])
+    }
+
+    return totalPayroll
+}
+
+// function allWagesFor(){
+//     let record = this;
+//     let datesWorked = record.timeOutEvents.map((e) => {return e['date']});
+//   //  console.log(`DATES WORKED: ${datesWorked}`);
+//     let uniqueDates = new Set(datesWorked);
+
+//     uniqueDates = [...uniqueDates];
+
+
+//     let salary = uniqueDates.reduce(function(total, dateString) {
+//  //       console.log(`${record.firstName} ${dateString} ${total + wagesEarnedOnDate(record, dateString)}`)
+//         return total + wagesEarnedOnDate.call(record, dateString);
+//     }, 0)
+
+//  //   console.log(salary);
+//     return salary
+// }
