83f290c9df7fc8068da27be6a70e86de4ee34533
github username <github email address>
2019-10-17 11:23:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,76 @@
 /* Your Code Here */
+let createEmployeeRecord = function(arr){
+    return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeArr){
+    return employeeArr.map(function(record) {
+        return createEmployeeRecord(record)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour,10),
+        date
+    })
+
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour,10),
+        date
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(searchDate){
+    let timeIn = this.timeInEvents.find(function(time){
+        return time.date === searchDate
+    })
+
+    let timeOut = this.timeOutEvents.find(function(time){
+        return time.date === searchDate
+    })
+
+    return (parseInt(timeOut.hour) - parseInt(timeIn.hour))/100
+}
+
+let wagesEarnedOnDate = function(searchDate){
+    let hoursWorked = hoursWorkedOnDate.call(this, searchDate);
+    // let hoursWorked = hoursWorkedOnDate(searchDate)
+    let payPerHour = this.payPerHour;
+
+    return hoursWorked * payPerHour
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName){
+        return srcArray.find(function(empRec){
+            return empRec.firstName === firstName
+        })
+}
+
+let calculatePayroll = function(empRecArr){
+    return empRecArr.reduce(function(total, empRec){
+        return total + allWagesFor.call(empRec)
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
